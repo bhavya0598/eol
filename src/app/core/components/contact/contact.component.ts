cc1be07e7703bb5e671f42041028d898a21ebd08
Bhavya <2679.stkabirnav@gmail.com>
2022-12-08 09:47:08 -0700

    --implemented contacted page

--- src/app/core/components/contact/contact.component.ts
+++ src/app/core/components/contact/contact.component.ts
@@ -50,6 +50,30 @@ export class ContactComponent implements OnInit {
       additionalText: 'earthoflight',
       link: 'https://us02web.zoom.us/j/2108839738',
     },
+    {
+      alt: 'Linkedin',
+      src: 'linkedin/linkedin.png',
+      additionalText: 'earthoflight',
+      link: 'https://www.linkedin.com/company/earth-of-light-therapeutic-network/',
+    },
+    {
+      alt: 'Google Business',
+      src: 'Google Business/Google Business.png',
+      additionalText: 'earthoflight',
+      link: 'https://www.google.com/maps/place/Earth+of+Light+Therapeutic+Services/@51.0916135,-115.3508799,17z/data=!3m1!4b1!4m5!3m4!1s0x53a023368d31d0ab:0x1ee1fe4e5b20b207!8m2!3d51.0916135!4d-115.3508799',
+    },
+    {
+      alt: 'Messanger',
+      src: 'Messanger/Messanger.png',
+      additionalText: 'earthoflight',
+      link: 'https://www.messenger.com/t/earthoflight',
+    },
+    {
+      alt: 'Telegram',
+      src: 'Telegram/Telegram.png',
+      additionalText: 'earthoflight',
+      link: 'https://telegram.me/earthoflight',
+    },
   ];
   constructor() {}
 
