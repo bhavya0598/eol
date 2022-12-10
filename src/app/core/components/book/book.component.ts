87f781ed7bfa29b84cafcf794f90635ee0904a31
Bhavya <2679.stkabirnav@gmail.com>
2022-12-09 20:10:19 -0700

    --fixed book page for mobile

--- src/app/core/components/book/book.component.ts
+++ src/app/core/components/book/book.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-book',
@@ -9,12 +9,17 @@ export class BookComponent implements OnInit {
   count = 2;
   owlPractice =
     'https://portal.owlpractice.ca/earthoflight/booking?therapist_id=1&location_id=1&rate_id=&day=2022-12-06&time=&video_session=0';
+  screenWidth: number = window.innerWidth;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenWidth = window.innerWidth;
+  }
   constructor() {}
 
   ngOnInit(): void {
     setInterval(() => {
       if (this.count != 0) this.count--;
-      else this.onClick();
+      // else this.onClick();
     }, 1000);
   }
   onClick() {
